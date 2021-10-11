67c5f9bf79e423fe9175fdb42fc7359bc33578fb
smilevalera <91686121+smilevalera@users.noreply.github.com>
2021-10-11 22:02:35 +0300

    Add files via upload
    я не понял в чём проблема

---
+++ Source.cpp
@@ -0,0 +1,45 @@
+﻿// valera gilev п43021
+#include <iostream>
+using namespace std;
+int main() {
+//Boolean8◦
+//.Даны два целых числа : A, B.Проверить истинность высказывания :
+//Каждое из чисел A и B нечетное»
+	/*int a, b;
+	bool res;
+	cin >> a >> b;
+	res = (a % 2 != 0 ) && ( b % 2 != 0);
+	cout << boolalpha;
+	cout << res << endl;
+
+	return 0;*/
+	//Boolean9◦
+	//. Даны два целых числа : A, B.Проверить истинность высказывания :
+	//«Хотя бы одно из чисел A и B нечетное».
+	/*int a, b;
+	bool res;
+	cin >> a >> b;
+	res = (a % 2 != 0) && (b % 2 == 0);
+	cout << boolalpha;
+	cout << res << endl;
+	return 0;*/
+	//Boolean11◦
+		//.Даны два целых числа : A, B.Проверить истинность высказывания : «Числа A и B имеют одинаковую четност
+	/*int a, b;
+	bool res;
+	cin >> a >> b;
+	res = (a % 2 == 0) && (b % 2 == 0);
+	cout << boolalpha;
+	cout << res << endl;
+	return 0;*/
+	//Boolean12◦
+		//.Даны три целых числа : A, B, C.Проверить истинность высказывания : «Каждое из чисел A, B, C положительное».
+	/*int a, b, c;
+	bool res;
+	cin >> a >> b >> c;
+	res = (a>0) && (b>0) && (c>0) ;
+	cout << boolalpha;
+	cout << res << endl;
+	return 0;*/
+
+}
